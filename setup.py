dadad45515693fa264e19eed433d7bad95e06eee
Nguyen Ho Duy Tri <duytri2712@gmail.com>
2020-04-18 10:15:47 +0700

    First commit.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+__author__ = 'Nguyen Ho Duy Tri'
+
+setup(
+    name='episode_mining',
+    version='0.0.1',
+    description='An inplementation of sequential patterm mining method',
+    author='Nguyen Ho Duy Tri',
+    author_email='trinhd@uit.edu.vn',
+    url='https://github.com/duytri/episode_mining',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    packages=['episode mining'],
+)
